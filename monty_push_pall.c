69d5aa028ca9679d25bdcfa585fd0a690a3c3c01
kemboi-73 <kemboimichael52@gmail.com>
2023-06-21 06:34:28 -0700

    s function that implements Implement the push and pall opcodes.

---
+++ monty_push_pall.c
@@ -0,0 +1,78 @@
+#include "monty.h"
+/**
+ * monty_push - pushes a value to a stack_t linked list
+ * @stack: a pointer to the top node of the stack(head)
+ * @line_number: the current working line number of a monty bytecodes file.
+ *
+ * Declare a tmp variable and new to rep new element
+ * Allocate memory to the new element
+ * insert new element
+ * STACK mode insert at front
+ * QUEUE mode insert at end
+ */
+void monty_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp, *new;
+	int i;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		set_op_tok_error(malloc_error());
+		return;
+	}
+
+	if (op_toks[1] == NULL)
+	{
+		set_op_tok_error(no_int_error(line_number));
+		return;
+	}
+	for (i = 0; op_toks[1][i]; i++)
+	{
+		if (op_toks[1][i] == '-' && i == 0)
+			continue;
+		if (op_toks[1][i] < '0' || op_toks[1][i] > '9')
+		{
+			set_op_tok_error(no_int_error(line_number));
+			return;
+		}
+	}
+	new->n = atoi(op_toks[1]);
+
+	if (check_mode(*stack) == STACK)	/*STACK mode*/
+	{
+		tmp = (*stack)->next;
+		new->prev = *stack;
+		new->next = tmp;
+		if (tmp)
+			tmp->prev = new;
+		(*stack)->next = new;
+	}
+	else	/*QUEUE mode*/
+	{
+		tmp = *stack;
+		while (tmp->next)
+			tmp = tmp->next;
+		new->prev = tmp;
+		new->next = NULL;
+		tmp->next = new;
+	}
+}
+
+/**
+ * monty_pall - prints values of a stack
+ * @stack: pointer to the top node of the stack
+ * @line_number: current working line of Monty bytecodes file
+ */
+
+void monty_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+	(void)line_number;
+}
